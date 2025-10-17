ba87cb703fe605b491b702cc895634d8e493ab3f
Wesley Lucas Cordeiro Fulgencio Taveira <wesley.fulgencio@AGU.GOV.BR>
2025-10-17 12:08:23 -0300

    feat(Cadastro-Inicio): inicio do cadastro de usuário

--- src/pages/CadastroUsuario/index.jsx
+++ src/pages/CadastroUsuario/index.jsx
@@ -1,30 +1,48 @@
 import './style.css'
-import Trash from '../../assets/trash.svg'
+import Trash from '@mui/icons-material/Delete'
 
 function CadastroUsuario() {
- 
+
+  const users = [
+    {
+      id: '1',
+      name: 'Carlos',
+      age: 33,
+      email: 'carlos@email.com,'
+
+    },
+    {
+      id: '2',
+      name: 'Marcos',
+      age: 25,
+      email: 'mar@email.com,'
+
+    }
+  ]
 
   return (
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='Nome' type='text'/>
-        <input name='idade' type='number'/>
-        <input name='email' type='email'/>
+        <input name='Nome' type='text' />
+        <input name='idade' type='number' />
+        <input name='email' type='email' />
         <button type='button'>Cadastrar</button>
       </form>
 
-      <div>
-        <div>
-          <p>Nome:</p>
-          <p>Idade:</p>
-          <p>Email:</p>
-        </div>
-        <button>
-          <img src={Trash}/>
-        </button>
 
-      </div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <div>
+            <p>Nome:{user.name}</p>
+            <p>Idade: {user.age}</p>
+            <p>Email: {user.email}</p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
 
 
     </div>
